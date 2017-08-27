fb229c9214920ddec4f35816beaf5b2ee36c95df
zoebisch <zoebisch@gmail.com>
2017-08-27 14:41:45 -0700

    imported user code from sinatra playlister, add user migration, and all path routing for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,48 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :index
   end
+
+  post '/signup' do
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect to "/"
+    else
+      if is_logged_in?(session)
+        redirect to "/tweets"
+      else
+        flash[:message] = "Invalid signup parameters"
+        redirect to "/signup"
+      end
+    end
+  end
+
+  get '/login' do
+    if is_logged_in?(session)
+      redirect to "/"
+    end
+    flash[:message] = "Please Log In"
+    erb :"/users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/"
+    else
+      flash[:message] = "Invalid username or password"
+      erb :"/users/login"
+    end
+  end
+
+  get '/logout' do
+    if is_logged_in?(session)
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 end
