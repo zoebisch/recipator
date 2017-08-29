59f741e379c41b86e9a6da5d61ee89a525d1ed38
zoebisch <zoebisch@gmail.com>
2017-08-28 19:04:52 -0700

    added route to make a new author, Flesh out ingredients routes

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,6 +9,25 @@ class AuthorsController < ApplicationController
     end
   end
 
+  get "/authors/new" do
+    if is_logged_in?(session)
+      erb :"/authors/new"
+    else
+      flash[:message] = "You must be logged in to add an author"
+      redirect to "/login"
+    end
+  end
+
+  post "/authors/new" do
+    if is_logged_in?(session)
+      @author = Author.find_or_create_by(params[:author])
+      redirect to "/authors/#{@author.slug}"
+    else
+      flash[:message] = "You must be logged in to add an author"
+      redirect to "/login"
+    end
+  end
+
   get "/authors/:slug" do
     if is_logged_in?(session)
       @author = Author.find_by_slug(params[:slug])
@@ -18,6 +37,7 @@ class AuthorsController < ApplicationController
       redirect to "/login"
     end
   end
+
 
   get "/authors/:slug/edit" do
     if is_logged_in?(session) && current_user(session).username == "site_admin" #Uniqueness established on creation
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,3 +1,45 @@
 class IngredientsController < ApplicationController
 
+  get "/ingredients" do
+    if is_logged_in?(session)
+      erb :"/ingredients/index"
+    else
+      flash[:message] = "You must be logged in view the ingredients"
+      redirect to "/login"
+    end
+  end
+
+  get "/ingredients/:slug" do
+    if is_logged_in?(session)
+      @ingredient = Ingredient.find_by_slug(params[:slug])
+      erb :"/ingredients/show"
+    else
+      flash[:message] = "You must be logged in view the ingredients"
+      redirect to "/login"
+    end
+  end
+
+  get "/ingredients/:slug/edit" do
+    if is_logged_in?(session) && current_user(session).username == "site_admin" #Uniqueness established on creation
+      @ingredient = Ingredient.find_by_slug(params[:slug])
+      erb :"/ingredients/edit"
+    else
+      flash[:message] = "You must be logged in view the ingredients"
+      redirect to "/login"
+    end
+  end
+
+  post "/ingredients/:slug/edit" do
+    if is_logged_in?(session) && current_user(session).username == "site_admin" #Uniqueness established on creation
+      @ingredient = Ingredient.find_by_slug(params[:slug])
+      binding.pry
+      @ingredient.update(params[:ingredient])
+
+      redirect to :"/ingredients/#{@ingredient.slug}"
+    else
+      flash[:message] = "You must be logged in to edit an ingredient"
+      redirect to "/login"
+    end
+  end
+
 end
